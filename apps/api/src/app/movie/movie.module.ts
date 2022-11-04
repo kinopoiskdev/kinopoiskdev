b23284af5ca87a09737a734959705fed50f64823
mdwitr0 <mdwit0r@gmail.com>
2022-11-05 00:33:08 +0100

    enable prisma logging

--- apps/api/src/app/movie/movie.module.ts
+++ apps/api/src/app/movie/movie.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
-import { PrismaModule } from '@prisma';
+import { loggingMiddleware, PrismaModule } from '@prisma';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule.forRoot({
+      prismaServiceOptions: {
+        explicitConnect: true,
+        prismaOptions: {
+          log: ['query', 'info', 'warn', 'error'],
+        },
+        middlewares: [loggingMiddleware()],
+      },
+    }),
+  ],
   controllers: [MovieController],
   providers: [MovieService],
 })
