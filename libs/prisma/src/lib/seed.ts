fb013fb8a6974a2f015078ec7248449cf07c8d86
mdwitr0 <mdwit0r@gmail.com>
2022-11-07 11:44:01 +0100

    add relations to seed

--- libs/prisma/src/lib/seed.ts
+++ libs/prisma/src/lib/seed.ts
@@ -1,8 +1,8 @@
 import * as dotenv from 'dotenv';
 import { PrismaClient } from './generated/prisma-client/index';
 import { Logger } from '@nestjs/common';
-import { movies, persons, personsFacts } from './mocks/movies.mock';
 import { join } from 'path';
+import { movies, persons, personsFacts } from './mocks/movies.mock';
 
 const prisma = new PrismaClient();
 
@@ -10,24 +10,79 @@ const logger = new Logger('Seed');
 
 async function main() {
   dotenv.config({ path: join(__dirname, '..', '..', '..', '..', '.env') });
-  logger.log('Movies run...');
-  await prisma.movie.createMany({ data: movies });
-  logger.log('Movies done');
-  logger.log('Persons run...');
-  const savedPerson = await prisma.person.create({ data: persons[0] });
-  logger.log('Persons done');
-  logger.log('PersonsFacts run...');
-  await prisma.personFact.createMany({
-    data: personsFacts.map((f) => ({ ...f, personId: savedPerson.id })),
-  });
-  logger.log('PersonsFacts done');
-  logger.log('Finish');
+  if (process.env.NODE_ENV === 'development') {
+    logger.log('Movies run...');
+    await prisma.movie.createMany({ data: movies });
+
+    const foundMovie = await prisma.movie.findFirst({ where: { kpId: 666 } });
+    const foundRelationMovie = await prisma.movie.findFirst({
+      where: { kpId: 435 },
+    });
+
+    logger.log('Update movie relations...');
+    await prisma.sequelsAndPrequel.create({
+      data: {
+        relationType: 'AFTER',
+        movie: {
+          connect: {
+            id: foundMovie.id,
+          },
+        },
+        relatedMovie: {
+          connect: {
+            id: foundRelationMovie.id,
+          },
+        },
+      },
+    });
+    await prisma.similarMovie.create({
+      data: {
+        movie: {
+          connect: {
+            id: foundMovie.id,
+          },
+        },
+        relatedMovie: {
+          connect: {
+            id: foundRelationMovie.id,
+          },
+        },
+      },
+    });
+    logger.log('Movies relations updated');
+    logger.log('Movies done');
+
+    logger.log('Persons run...');
+    const savedPerson = await prisma.person.create({ data: persons[0] });
+    logger.log('Persons done');
+
+    logger.log('PersonsFacts run...');
+    await prisma.personFact.createMany({
+      data: personsFacts.map((f) => ({ ...f, personId: savedPerson.id })),
+    });
+    logger.log('PersonsFacts done');
+    logger.log('Person relations run...');
+    const foundPersons = await prisma.person.findMany({ where: {} });
+
+    for (const foundPerson of foundPersons) {
+      await prisma.movieOnPerson.create({
+        data: {
+          kpId: foundPerson.kpId,
+          kpMovieId: foundMovie.kpId,
+          movie: { connect: { id: foundMovie.id } },
+          person: { connect: { id: foundPerson.id } },
+          description: 'description',
+          profession: ['ACTOR'],
+        },
+      });
+    }
+    logger.log('Person relations updated');
+
+    logger.log('Finish');
+  }
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
   .catch((e) => logger.error(e))
   .finally(async () => {
     await prisma.$disconnect();
